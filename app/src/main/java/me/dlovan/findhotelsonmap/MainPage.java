60a76b93948baaa009171c29ff3f4bfbcfd0493a
dlovan ali <dlovan.bashir.ali@gmail.com>
2019-01-17 16:37:45 +0300

    Location Services integrated with permisions assined

--- app/src/main/java/me/dlovan/findhotelsonmap/MainPage.java
+++ app/src/main/java/me/dlovan/findhotelsonmap/MainPage.java
@@ -1,7 +1,20 @@
 package me.dlovan.findhotelsonmap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +23,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainPage extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +45,114 @@ public class MainPage extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         mMap = googleMap;
-        LatLng Duhok = new LatLng(36.868333, 42.955599);
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(36.8544595, 42.9248687);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Center Of Duhok"));
-        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Duhok, 17));
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("My Locations: ", location.toString());
+                mMap.clear(); // clears our map
+
+                // mMap.addMarker(new MarkerOptions().position(location.getLatitude(), location.getLongitude(), 1));
+
+                // Add a marker in Sydney and move the camera
+                LatLng newLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(newLocation).title("New Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLocation, 10));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(),
+                            location.getLongitude(), 1
+                    );
+
+                    String fullAddress = "";
+
+                    if (addressList != null && addressList.size() > 0) {
+
+                        Log.d("Address: ", addressList.get(0).toString());
+
+//                        Toast.makeText(MapsActivity.this, addressList.get(0).getAddressLine(0), Toast.LENGTH_LONG)
+//                                .show();
+
+                        if (addressList.get(0).getAddressLine(0) != null) {
+                            fullAddress += addressList.get(0).getAddressLine(0) + " ";
+
+                        }
+                        if (addressList.get(0).getSubAdminArea() != null) {
+                            fullAddress += addressList.get(0).getSubAdminArea() + " ";
+                        }
+
+                        Toast.makeText(MainPage.this, "Address+" + fullAddress, Toast.LENGTH_LONG)
+                                .show();
+
+                    } else {
+                        Log.d("Address:", "Couldn't find Address");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT < 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+                //Ask for permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            } else {
+                // we have permission!
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0]
+                == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            )
+                    == PackageManager.PERMISSION_GRANTED)
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2, 2, locationListener);
+
+        }
+
     }
 
 }
